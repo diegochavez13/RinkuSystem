93968d2c3a4064ea587c9c8e71aa6964aca04d59
diaegochavez13 <diego.chavez13@hotmail.com>
2019-03-15 18:39:30 -0700

    Agregara Diagramas y correcciones

--- scripts/Tablas/CatTrabajador.sql
+++ scripts/Tablas/CatTrabajador.sql
@@ -1,8 +1,6 @@
 CREATE TABLE CatTrabajador(
 				iID int Primary Key Identity (1,1),
 				nvNombre Nvarchar(50) Not Null,
-				nvApellidoPaterno Nvarchar(50) Not Null,
-				nvApellidoMaterno Nvarchar(50),
 				iRolTrabajador Int Not Null,
 				iTipoTrabajador Int Not Null,
 				btEstatus bit Default 1,
